81b890cbfea9670e5ac2bf06099c778106813638
Karen Li <karenli@dyn232218.cc.lehigh.edu>
2023-10-25 17:58:02 -0400

    updated ModTransiever

--- ModSender/ModTransiever.py
+++ ModSender/ModTransiever.py
@@ -1,12 +1,12 @@
 from joystickHandler import JoystickHandler
 from ESPNOW import ESPNOWControl
 from robotConfig import RobotConfig
-from simpleGUI import SimpleGUI
 import time
+import math
 
 #ESPNOW PARAMS
 ESP_VERBOSE = True
-PORT = "/dev/cu.wchusbserial1130" #serial port for the transiever
+PORT = "/dev/cu.usbmodem1101"
 LIST_OF_MAC_ADDRESS = [
     "34:85:18:91:BC:94",
     "34:85:18:91:BE:34",
@@ -18,22 +18,19 @@ LIST_OF_MAC_ADDRESS = [
     "DC:54:75:D7:F7:FC", #7 hanqing
     "48:27:E2:E6:E6:44", #8 kim
     "34:85:18:91:24:F0", #9
-    
-    
+    "34:85:18:91:20:a8", #10 Leo
 ]
 
-# MASTER_MAC = "34:85:18:91:C7:80" #address of the transiever
-MASTER_MAC = "C0:49:EF:EB:FE:34"
+MASTER_MAC = "34:85:18:91:BC:94" #address of the transiever
 
 SLAVE_INDEX = 7 #-1 means broadcast
-
 
 BRODCAST_CHANNEL = 1 # SLAVE_INDEX will override this value if SLAVE_INDEX is not -1
 
 
 joyhandler = JoystickHandler()
 esp_now = ESPNOWControl(PORT, LIST_OF_MAC_ADDRESS, ESP_VERBOSE)
-robConfig = RobotConfig(esp_now, "./robot_configs.json")
+robConfig = RobotConfig(esp_now, "robot_configs.json")
 
 #set configs for all slave indexes that you want to use 
 #bicopter basic contains configs for a robot with no feedback
@@ -43,20 +40,52 @@ robConfig.sendAllFlags(BRODCAST_CHANNEL, SLAVE_INDEX, "bicopterbasic")
 robConfig.startBNO(BRODCAST_CHANNEL, SLAVE_INDEX)
 robConfig.startBaro(BRODCAST_CHANNEL, SLAVE_INDEX)
 robConfig.startTranseiver(BRODCAST_CHANNEL, SLAVE_INDEX, MASTER_MAC)
-mygui = SimpleGUI()
+
+# Nicla vision control
+des_yaw = 0
+des_height = 0
+control_yaw = 0
+control_height = 0
+Ky1 = 1
+Ky2 = 1
+Kh1 = 1
+Kh2 = 1
+gamma1 = 0.8
+gamma2 = 0.8
+max_x = 240
+max_y = 160
+x, y, w, h = -1, -1, -1, -1
 
 
 y = False
 try:
+    time_prev = time.time()
     while not y:
-
+        # under joystick control
         outputs, y = joyhandler.get_outputs()
-        #outputs = [0]*13
-        feedback  = esp_now.getFeedback()
-        mygui.update_interface(feedback[1][3], outputs[6], feedback[1][0], outputs[3])
+        fx = outputs[1]
+        #values from the Nicla
+        feedback = esp_now.getFeedback() 
+        _x = feedback[1][2] * 1000
+        _y = feedback[1][3] * 1000
+        _w = feedback[1][4] * 1000
+        _h = feedback[1][5] * 1000
+        if _x == -1 or _y == -1 or _w == -1 or _h == -1:
+            continue
+        if x != _x or y != _y or w != _w or h != _h:
+            _x, _y, _w, _h = x, y, w, h
+            des_yaw = des_yaw*gamma1 + (((x - max_x/2)/max_x)*math.pi/2)*(1-gamma1)
+            des_height = des_height*gamma2 + ((y - max_y/2)/max_y)*(1-gamma2)
+        dt = time.time() - time_prev
+        control_yaw += Ky1 * des_yaw * dt
+        des_yaw -= Ky2 * des_yaw * dt
+        control_height += Kh1 * des_height * dt
+        des_height -= Kh2 * des_height * dt
+        print("fx", fx)
+        print("control_yaw", control_yaw)
+        print("control_height", control_height)
         esp_now.send([21] + outputs[:-1], BRODCAST_CHANNEL, SLAVE_INDEX)
-        #print(feedback)
-        
+        time_prev = time.time()
         time.sleep(0.02)
 except KeyboardInterrupt:
     print("Loop terminated by user.")
