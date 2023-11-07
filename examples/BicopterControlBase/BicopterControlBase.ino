3eae56d120fd40c25e1171131dec2e6742e993d4
nuryte <positronic83@gmail.com>
2023-11-07 15:57:47 -0500

    readded multi compatability

--- examples/BicopterControlBase/BicopterControlBase.ino
+++ examples/BicopterControlBase/BicopterControlBase.ino
@@ -356,9 +356,9 @@ void loop() {
     counter2 = 0;
     if (transceiverEnabled){
       
-      blimp.IBus.loop();
+      // blimp.IBus.loop();
 
-      espSendData1.flag = 1;
+      // espSendData1.flag = 1;
       espSendData1.values[0] = sensors.estimatedZ - sensors.groundZ;
       espSendData1.values[1] = sensors.yaw;
       espSendData1.values[2] = sensorData.values[0];
@@ -366,14 +366,14 @@ void loop() {
       espSendData1.values[4] = outputs.m1;
       espSendData1.values[5] = outputs.s1;
       blimp.send_esp_feedback(transceiverAddress, &espSendData1);
-      espSendData2.flag = 2;
-      espSendData2.values[0] = (float)blimp.IBus.readChannel(5);
-      espSendData2.values[1] = (float)blimp.IBus.readChannel(6);
-      espSendData2.values[2] = (float)blimp.IBus.readChannel(7);
-      espSendData2.values[3] = (float)blimp.IBus.readChannel(8);
-      espSendData2.values[4] = controls.tz;
-      espSendData2.values[5] = battery_level;
-      blimp.send_esp_feedback(transceiverAddress, &espSendData2);
+      // espSendData2.flag = 2;
+      // espSendData2.values[0] = (float)blimp.IBus.readChannel(5);
+      // espSendData2.values[1] = (float)blimp.IBus.readChannel(6);
+      // espSendData2.values[2] = (float)blimp.IBus.readChannel(7);
+      // espSendData2.values[3] = (float)blimp.IBus.readChannel(8);
+      // espSendData2.values[4] = controls.tz;
+      // espSendData2.values[5] = battery_level;
+      // blimp.send_esp_feedback(transceiverAddress, &espSendData2);
     }
   }
 
