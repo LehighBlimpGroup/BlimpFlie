26c0f92a3ba836a849cb468c4473115277fe9d20
diegosdantonio <diego.s.dantonio@gmail.com>
2023-11-08 19:43:56 -0500

    Zigzag onboard working perfect, toggle button shutdown the robot now

--- ModSender/MultiTransiever.py
+++ ModSender/MultiTransiever.py
@@ -28,7 +28,7 @@ esp_now = ESPNOWControl(PORT, LIST_OF_MAC_ADDRESS, ESP_VERBOSE)
 # Load robot configuration
 robotConfigs = [RobotConfig(esp_now, i, robot_mac) for i, robot_mac in enumerate(LIST_OF_MAC_ADDRESS)]
 # GUIS
-sensor_guis = [SensorGUI(GUI_ENABLED,robConfig=robConfig) for robConfig in robotConfigs]
+sensor_guis = [SensorGUI(GUI_ENABLED,robConfig=robConfig, esp_now=esp_now) for robConfig in robotConfigs]
 
 # Send flags to each robot
 for robConfig in robotConfigs:
@@ -47,29 +47,45 @@ y_pressed = False
 try:
     while not y_pressed:
         outputs, y_pressed, a_key_pressed = joyhandler.get_outputs(yaw_mode=JOYSTICK_YAW_MODE)  # get joystick input
-        #outputs[8] = int(a_key_pressed)
-        
+
         # For each robot
         for i, robotConfig in enumerate(robotConfigs):
             feedback = esp_now.getFeedback(i)  # get sensor data from robot
-            # nicla = esp_now.getFeedback(2)  # get sensor data from robot
+
+            #  # ------- Autonomous mode ----------
+            # if a_key_pressed:
+            #     des_fx, des_z, des_yaw = behavior_robots[i].execute(feedback)
+            #     outputs[1] = des_fx  # Forward
+            #     outputs[3] = des_z  # Z
+            #     outputs[6] = des_yaw  # Yaw control
 
              # ------- Autonomous mode ----------
             if a_key_pressed:
-                des_fx, des_z, des_yaw = behavior_robots[i].execute(feedback)
-                outputs[1] = des_fx  # Forward
-                outputs[3] = des_z  # Z
-                joyhandler.tz = des_yaw  # Yaw control
-
+                # outputs[6] = 0
+                outputs[8] = 2 # send 2 for randomwalk
+                outputs[9] = 1
+                print(outputs)
+                print("autonomous")
+                # des_fx, des_z, des_yaw = behavior_robots[i].execute(feedback)
+                # outputs[1] = des_fx  # Forward
+                # outputs[3] = des_z  # Z
+                # joyhandler.tz = des_yaw  # Yaw control
+            else:
+                outputs[8] = 0  # send 0 for normal flight
+                outputs[9] = 0
+                print(outputs)
 
             # Display sensors and output
             # sensor_guis[i].update_nicla_box(nicla[0], 160 - nicla[1], nicla[2], nicla[3], 240, 160)
             sensor_guis[i].update_interface(feedback[1], outputs[6], feedback[0], outputs[3], feedback[2], feedback[3])  # display sensor data
 
-            # Send message to all robots
-            esp_now.send([21] + outputs[:-1], BRODCAST_CHANNEL, robotConfig.slave_index)  # send control command to robot
 
+            if sensor_guis[i].toggle_power:
 
+                # Send message to all robots
+                esp_now.send([21] + outputs[:-1], BRODCAST_CHANNEL, robotConfig.slave_index)  # send control command to robot
+            else:
+                esp_now.send([21, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], BRODCAST_CHANNEL, robotConfig.slave_index)
         # time.sleep(0.02)
         sensor_guis[0].sleep(0.02)
 
