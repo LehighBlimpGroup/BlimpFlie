84903ad1f6c7e91ed167af2ae91239744b0fefa8
Jiawei <cyandicnx@hotmail.com>
2023-11-08 21:16:40 -0500

    Changed niclaControl.ino to integrate balloon and goal tracking

--- examples/BicopterControlBase/niclaControl.ino
+++ examples/BicopterControlBase/niclaControl.ino
@@ -1,15 +1,25 @@
-
-
-
-
+// Camera sensor related variables
 float max_x = 240;
 float max_y = 160;
-float _x, _y, _w, _h;
+float detection_x = 0;
+float detection_y = 0; 
+float detection_w = 0; 
+float detection_h = 0;
+float tracking_x = 0;
+float tracking_y = 0;
+float tracking_w = 0; 
+float tracking_h = 0;
+float last_detection_x = 0;
+float last_detection_y = 0;
+float last_detection_w = 0;
+float last_detection_h = 0;
+float last_tracking_x = 0;
+float last_tracking_y = 0;
+float last_tracking_w = 0;
+float last_tracking_h = 0;
+float tof_dist = 0;
+
 float des_yaw, _yaw, Ky, _height;
-float x = 0;
-float y = 0;
-float w = 0;
-float h = 0;
 float control_yaw = 0;
 float control_height = 0;
 float control_fx = 0;
@@ -38,34 +48,43 @@ void addNiclaControl(controller_t *controls, sensors_t *sensors, ModBlimp *blimp
 
   */
   blimp->IBus.loop();
-  
-  _x = (float)blimp->IBus.readChannel(1);
-  _y = (float)blimp->IBus.readChannel(2);
-  _w = (float)blimp->IBus.readChannel(7);
-  _h = (float)blimp->IBus.readChannel(8);
-  _dist = (float)blimp->IBus.readChannel(9); //tof
+  int nicla_flag = (int)blimp->IBus.readChannel(0);
+  tracking_x = (float)blimp->IBus.readChannel(1);
+  tracking_y = (float)blimp->IBus.readChannel(2);
+  tracking_w = (float)blimp->IBus.readChannel(3);
+  tracking_h = (float)blimp->IBus.readChannel(4);
+  detection_x = (float)blimp->IBus.readChannel(5);
+  detection_y = (float)blimp->IBus.readChannel(6);
+  detection_w = (float)blimp->IBus.readChannel(7);
+  detection_h = (float)blimp->IBus.readChannel(8);
+  tof_dist = (float)blimp->IBus.readChannel(9);
   _yaw = sensors->yaw;
   _height = sensors->estimatedZ - sensors->groundZ;
 
-
-  if (init_bool){
+  if (init_bool) {
     des_height = controls->fz;
     height_diff = controls->fz - _height;
   }
 
-  if (_x == 0 && _y == 0 && _w == 0 && _h == 0) {
-    detected = false;
-  } else if (x != _x || y != _y || w != _w || h != _h) {
-    
-    float x_cal = (x / max_x); 
+
+  if (nicla_flag != 3) { 
+    // Some Detection
+    detected = true;
+  }
+
+  if (last_tracking_x != tracking_x || last_tracking_y != tracking_y || last_detection_w != detection_w || last_detection_h != detection_h) {  
+    float x_cal = tracking_x / max_x; 
+    float y_cal = tracking_y / max_y;
     des_yaw = ((x_cal - 0.5) * nicla_tuning->x_cal_weight);
     robot_to_goal = _yaw - des_yaw; 
+    detected = true;
+  }
+    
+  if (nicla_flag == 1) {
     float relative_to_goal = nicla_tuning->goal_theta_back - robot_to_goal;
     relative_to_goal = atan2(sin(relative_to_goal), cos(relative_to_goal));
     goal_act = nicla_tuning->goal_theta_back;
-    
-    
-    
+  
     if (abs(relative_to_goal) > 3*PI/4) { // seeing front goal
       goal_act = goal_act + PI;
       goal_act = atan2(sin(goal_act), cos(goal_act));
@@ -73,20 +92,17 @@ void addNiclaControl(controller_t *controls, sensors_t *sensors, ModBlimp *blimp
       control_yaw = atan2(sin(control_yaw), cos(control_yaw));
       last_front_goal_time = millis();
       // changeHeight(y, _height, nicla_tuning);
-      detected = true;
-    }
-    else if (abs(relative_to_goal) > PI/2){ //seeing noise
+    } else if (abs(relative_to_goal) > PI/2){ //seeing noise
       last_noise_time = millis();
-      
     } else{ // seeing back goal
       last_back_goal_time = millis();
       goal_act = atan2(sin(goal_act), cos(goal_act));
       control_yaw = robot_to_goal * nicla_tuning->goal_ratio - goal_act * (1 - nicla_tuning->goal_ratio);
       control_yaw = atan2(sin(control_yaw), cos(control_yaw));
       // changeHeight(y, _height, nicla_tuning);
-      detected = true;
     }
-  } 
+  }
+  
   // else {// nicla has seen something, but not right now}
 
   if (detected == false) {
@@ -94,7 +110,7 @@ void addNiclaControl(controller_t *controls, sensors_t *sensors, ModBlimp *blimp
   } else {
     random_spiral = nicla_tuning->max_move_x; //reset spiral for random walk
     
-    if (max(w, h) > nicla_tuning->goal_dist_thresh){
+    if (max(last_detection_w, last_detection_h) > nicla_tuning->goal_dist_thresh){
       fullCharge(controls, nicla_tuning);
     } else{
       
@@ -108,10 +124,10 @@ void addNiclaControl(controller_t *controls, sensors_t *sensors, ModBlimp *blimp
   controls->tz = control_yaw;
   controls->fx = control_fx;
   
-  x = _x;
-  y = _y;
-  w = _w;
-  h = _h;
+  last_tracking_x = tracking_x;
+  last_tracking_y = tracking_y;
+  last_detection_w = detection_w;
+  last_detection_h = detection_h;
 }
 
 
@@ -159,9 +175,9 @@ void fullCharge(controller_t *controls,  nicla_tuning_s *nicla_tuning) {
 }
 
 void changeHeight(float y, float _height,  nicla_tuning_s *nicla_tuning) {
-  float y_cal = (y / max_y); 
-  float h_cal = (h / max_y);
-  if (max(w, h) < nicla_tuning->goal_dist_thresh && max(w, h) > 20){
+  float y_cal = (last_tracking_y / max_y); 
+  float h_cal = (last_detection_h / max_y);
+  if (max(last_detection_w, last_detection_h) < nicla_tuning->goal_dist_thresh && max(last_detection_w, last_detection_h) > 20){
     // if (y_cal - h_cal/2 > nicla_tuning->height_threshold || y_cal + h_cal/2 < nicla_tuning->height_threshold) {
     des_height = des_height - ((y_cal - nicla_tuning->height_threshold) * nicla_tuning->height_strength)*((float)dt)/1000000.0f;
   }
