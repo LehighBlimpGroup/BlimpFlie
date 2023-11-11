86f9d2003f57e44af9c538ccac583222b9445527
Karen <kkl2242@lehigh.edu>
2023-11-11 15:24:21 -0500

    autonomous flying backwards

--- ModSender/parameters.py
+++ ModSender/parameters.py
@@ -12,9 +12,9 @@ PORT = "/dev/cu.usbmodem1301" #serial port for the transiever
 LIST_OF_MAC_ADDRESS = [
 #     # "DC:54:75:D8:01:04", #0 KkL Sender
 #     # "34:85:18:91:B5:B4", #1 KKL Back-up sender
-    # "48:27:E2:E6:EC:CC", #2 Sensor test drone
+#     # "48:27:E2:E6:EC:CC", #2 Sensor test drone
 #     # "48:27:E2:E6:E4:0C", #3 Big diego drone first
-      "48:27:E2:E6:DF:A0" #4 KKL Nicla drone
+    #   "48:27:E2:E6:DF:A0" #4 KKL Nicla drone
 #     # "48:27:E2:E6:ED:24", #5 bingxu
 #     # "48:27:E2:E6:DE:3C", #6
 #     # "DC:54:75:D7:F7:FC", #7 hanqing
@@ -25,9 +25,10 @@ LIST_OF_MAC_ADDRESS = [
     # "34:85:18:8F:36:B0", # Diego
     # "48:27:E2:E6:ED:24", # Bella
     # "34:85:18:91:BC:94" # Joy
+    "34:85:18:8D:8B:38" # KKL 2.0
 ]
 
-MASTER_MAC = "34:85:18:91:B5:B4"#address of the transceiver
+MASTER_MAC = "34:85:18:91:B5:B4" #address of the transceiver
 BRODCAST_CHANNEL = 1             # SLAVE_INDEX will override this value if SLAVE_INDEX is not -
 
 JOYSTICK_YAW_MODE = 0
