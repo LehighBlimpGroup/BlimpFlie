37412178886062c3cd734163bcec53f6cc6fc9a9
Hanqing Qi <billy.qi03@gmail.com>
2023-11-13 12:13:12 -0500

    📥 Add `ModSender/gui/sensorGUI2.py` Support
    -.

--- ModSender/MultiTransiever.py
+++ ModSender/MultiTransiever.py
@@ -4,18 +4,38 @@ from parameters import *
 from teleop.joystickHandler import JoystickHandler
 from comm.ESPNOW import ESPNOWControl
 from robot.robotConfig import RobotConfig
-from gui.visualizer import SensorGUI
+from gui.sensorGUI2 import SensorGUI2
 
 
 # Override feedback params for multiple robots
 YAW_SENSOR = True
 Z_SENSOR = True
 
+def sensorgui2_reconnect_callback(id):
+    robotConfigs[id].initialize_system()
+    print("Reconnecting to robot %d: " % robotConfigs[id].slave_index, robotConfigs[id].mac)
 
-# User interface
+def sensorgui2_close_callback(id):
+    print("Closing connection to robot %d: " % robotConfigs[id].slave_index, robotConfigs[id].mac)
+    esp_now.send([21, 0,0,0,0,0,0,0,0,0,0,0,0], BRODCAST_CHANNEL, robotConfigs[id].slave_index)    
 
+def sensorgui2_send_flag_callback(id, flags):
+    print("Sending flags to robot %d: " % robotConfigs[id].slave_index, robotConfigs[id].mac)
+    deterministic_walk = robotConfigs[id].get_config(ROBOT_JASON)['deterministic_walk']
+    force_x, z_level, time_to_rotate, angle, zigzag = flags
+    print("Flags: ", flags)
+    # [deterministic_walk["SWITCH_TIME"],
+    # deterministic_walk["NUM_ZIGS"],
+    # deterministic_walk["Z_LEVEL"],
+    # deterministic_walk["TIME_ROTATE"],
+    # deterministic_walk["ANGLE_THRESH"],
+    # deterministic_walk["STEP_ZIG_ZAG"],
+    # deterministic_walk["FORWARD_FORCE"]] = [10, 5, z_level, time_to_rotate, angle, zigzag, force_x]
 
-
+    deterministic_walk["FORWARD_FORCE"] = force_x
+    deterministic_walk["STEP_ZIG_ZAG"] = zigzag
+    deterministic_walk["SWITCH_TIME"] = time_to_rotate * 1000
+    robotConfigs[id].send_flags(read_file=False)
 
 # Joystick
 joyhandler = JoystickHandler(yaw_sensor=YAW_SENSOR)
@@ -28,7 +48,7 @@ esp_now = ESPNOWControl(PORT, LIST_OF_MAC_ADDRESS, ESP_VERBOSE)
 # Load robot configuration
 robotConfigs = [RobotConfig(esp_now, i, robot_mac) for i, robot_mac in enumerate(LIST_OF_MAC_ADDRESS)]
 # GUIS
-sensor_guis = [SensorGUI(GUI_ENABLED,robConfig=robConfig, esp_now=esp_now) for robConfig in robotConfigs]
+sensor_guis = [SensorGUI2(GUI_ENABLED, id, sensorgui2_reconnect_callback, sensorgui2_close_callback, sensorgui2_send_flag_callback) for id in range(len(robotConfigs))]
 
 # Send flags to each robot
 for robConfig in robotConfigs:
@@ -77,10 +97,10 @@ try:
 
             # Display sensors and output
             # sensor_guis[i].update_nicla_box(nicla[0], 160 - nicla[1], nicla[2], nicla[3], 240, 160)
-            sensor_guis[i].update_interface(feedback[1], outputs[6], feedback[0], outputs[3], feedback[2], feedback[3])  # display sensor data
+            sensor_guis[i].update(feedback[1], outputs[6], feedback[0], outputs[3], feedback[3], feedback[2], True)  # display sensor data
 
 
-            if sensor_guis[i].toggle_power:
+            if sensor_guis[i].use_toggled_connect:
 
                 # Send message to all robots
                 esp_now.send([21] + outputs[:-1], BRODCAST_CHANNEL, robotConfig.slave_index)  # send control command to robot
