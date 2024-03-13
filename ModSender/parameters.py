e1e316ed6a5921a817acc11c70a50c19a2beba5b
diegosdantonio <diego.s.dantonio@gmail.com>
2024-03-13 15:57:54 -0400

    Skynetnew

--- ModSender/parameters.py
+++ ModSender/parameters.py
@@ -4,50 +4,20 @@ ROBOT_JASON = "bicopterbasic"
 #ESPNOW PARAMS
 # MASTER_MAC = "34:85:18:91:C7:80" #address of the transceiver
 ESP_VERBOSE = False
-PORT = "COM4" #serial port for the transiever
+PORT = "COM10" #serial port for the transiever
 
 
 LIST_OF_MAC_ADDRESS = [
-#     # "34:85:18:91:BC:94",
-#     # "34:85:18:91:BE:34",
-#     # "48:27:E2:E6:EC:CC", #2 Sensor test drone
-#     # "48:27:E2:E6:E4:0C", #3 Big diego drone first
-#     # "48:27:E2:E6:DF:A0", #4 KKL Nicla drone
-#     # "48:27:E2:E6:ED:24", #5 bingxu
-#     # "48:27:E2:E6:DE:3C", #6
-#     # "DC:54:75:D7:F7:FC", #7 hanqing
-#     # "48:27:E2:E6:E6:44", #8 kim
-#     # "34:85:18:91:24:F0", #9
-#     # "48:27:E2:E6:E4:0C", #10 Big Wall
-#     "48:27:E2:E6:E1:00", #11 david
-    # "34:85:18:8F:36:B0" # Diego
-#     "34:85:18:8F:36:B0", # Diego
-#     "34:85:18:91:CF:40"
-# From here
-#    "34:85:18:91:CF:40"     #     "48:27:E2:E6:E6:50", #1 check
-#     "48:27:E2:E6:E5:64", #2 check
-#     "48:27:E2:E6:E1:00", #3 check final
-# #     "34:85:18:91:BD:B0", #4 no working,MM,
-# #     "34:85:18:91:BC:94", #5
-# #     "48:27:E2:E6:E0:1C", #6 check
-#     "34:85:18:91:C1:2C",#7 final
-#     "34:85:18:AC:B5:20", #8 check
-#     "34:85:18:AC:0E:30", #9
-#     "34:85:18:8D:A0:D4", #10
-    # "34:85:18:AC:0E:30"  #11
-    # "DC:54:75:D7:F7:A4",
-    # "34:85:18:AB:FE:68"
-    # "34:85:18:AB:FE:68" #NEW_RING
-    "48:27:E2:E6:E0:1C",
-    # "34:85:18:AB:FE:68"
-    # "34:85:18:AC:BB:70"
+    "34:85:18:AC:0E:30"
+    # "48:27:E2:E6:E0:1C"
 ]
 
-MASTER_MAC = "34:85:18:8D:86:70" #"C0:49:EF:E3:34:78"
+
+MASTER_MAC = "DC:DA:0C:57:4B:9C" #"C0:49:EF:E3:34:78"
 BRODCAST_CHANNEL = 1 # SLAVE_INDEX will override this value if SLAVE_INDEX is not -1
 
 
-JOYSTICK_YAW_MODE = 1
+JOYSTICK_YAW_MODE = 0
 GUI_ENABLED = True
 
 
